b53d084fa0fe3e09cf1a4c515187d14359e5a414
Bryan Workman <bryan@bryans-mbp.lan>
2021-04-23 09:50:54 -0700

    adds comments to code

--- Task/Task/Controllers/View Controllers/TaskDetailViewController.swift
+++ Task/Task/Controllers/View Controllers/TaskDetailViewController.swift
@@ -9,37 +9,53 @@ import UIKit
 
 class TaskDetailViewController: UIViewController {
 
+    //MARK: - Outlets
     @IBOutlet weak var taskNameTextField: UITextField!
     @IBOutlet weak var taskNotesTextView: UITextView!
     @IBOutlet weak var taskDueDatePicker: UIDatePicker!
     
+    //MARK: - Properties
+    //landing pad
     var task: Task?
+    //optional date value...will only get assigned if user selects a date
     var date: Date?
     
+    //MARK: - Lifecycle Methods
+    //when view gets loaded, call updateViews() function to update outlets to display information on a task if one got passed in
     override func viewDidLoad() {
         super.viewDidLoad()
         updateViews()
     }
     
+    //MARK: - Actions
     @IBAction func saveButtonTapped(_ sender: Any) {
+        //Making sure that we have text
         guard let name = taskNameTextField.text, !name.isEmpty else { return }
+        
+        //If we have a task we are going to update it
         if let task = task {
             TaskController.shared.update(task: task, name: name, notes: taskNotesTextView.text, dueDate: date)
+            
+        //if we don't have an entry we are going to create one
         } else {
             TaskController.shared.createTaskWith(name: name, notes: taskNotesTextView.text, dueDate: date)
         }
+        //Removes the top view off of our Navigation Stack
         navigationController?.popViewController(animated: true)
     }
     
-    
+    //if user selects a date, the date property will be assigned the selected date from the date picker
     @IBAction func dueDatePickerDateChanged(_ sender: Any) {
         self.date = taskDueDatePicker.date
     }
     
+    //MARK: - Helper Methods
+    //if a task gets passed in to TaskDetailViewController, we will unwrap the task, then update our outlets to display the name, notes, and date of the task.
     func updateViews() {
         guard let task = task else { return }
         taskNameTextField.text = task.name
         taskNotesTextView.text = task.notes
         taskDueDatePicker.date = task.dueDate ?? Date()
     }
-}
+    
+} //End of class
--- Task/Task/Views/Custom Cell/TaskTableViewCell.swift
+++ Task/Task/Views/Custom Cell/TaskTableViewCell.swift
@@ -7,28 +7,61 @@
 
 import UIKit
 
+/**
+The protocol we will use to handle the update of the cell when the `completionButton` is toggled
+
+Step One:
+   - Define the protocol. This will need to interact with class level objects and define the task we want our delegate to handle.
+
+Delegate Methods:
+   - taskCelllButtonTapped(_ sender: TaskTableViewCell)
+
+*/
 protocol TaskCompletionDelegate: AnyObject {
+    /**
+    Delegate method for the `TaskCompletionDelegate`
+
+    Parameters:
+       - sender: The cell that that user toggled
+    */
     func taskCellButtonTapped(_ sender: TaskTableViewCell)
 }
 
 class TaskTableViewCell: UITableViewCell {
 
+    //MARK: - Outlets
     @IBOutlet weak var taskNameLabel: UILabel!
     @IBOutlet weak var completionButton: UIButton!
     
+    //MARK: - Properties
+    //landing pad
     var task: Task? {
+        //didSet will get triggered anytime task is assigned a value.
         didSet {
+            //when task gets set, we want to update the views with information from that task
             updateViews()
         }
     }
+    /**
+    The `delegate` or *intern* for the protocol `TaskCompletionDelegate`
+
+    - weak: We mark this method as weak to not create a retain cycle
+    - optional: We do not want to set the value of the delegate here.
+    */
     weak var delegate: TaskCompletionDelegate?
     
+    //MARK: - Actions
     @IBAction func completionButtonTapped(_ sender: Any) {
         if let delegate = delegate {
+            /// This is the call to action for the delegate. Hey intern, go get me a coffee
             delegate.taskCellButtonTapped(self)
         }
     }
     
+    //MARK: - Helper Methods
+    /**
+     - Description: If we have a Task then the user wants to update or view that Task. In order to allow them to do that we are going to display the data from our passed task. If the task's isComplete property is set to true, we will display our "complete" image..otherwise, we want the image to be our "incomplete" image
+     */
     func updateViews() {
         guard let task = task else { return }
         taskNameLabel.text = task.name
@@ -38,4 +71,5 @@ class TaskTableViewCell: UITableViewCell {
             completionButton.setBackgroundImage(UIImage(named: "incomplete"), for: .normal)
         }
     }
-}
+    
+} //End of class
